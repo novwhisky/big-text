2275d671317d1fc97d0ef96ce9b53952a6b74b5c
Nathan Witt <wittnl@gmail.com>
2016-05-25 00:39:23 -0500

    BigText fixes, tests on Chrome/Android

--- fluid-text.js
+++ fluid-text.js
@@ -3,9 +3,9 @@ var fudge = 1.05;
 function resizeText(e) {
   var $txt = e.target;
   var charlen = $txt.innerText.length || 1;
-  var txtArea = window.innerHeight * window.innerWidth;
+  var txtArea = document.documentElement.clientHeight * document.documentElement.clientWidth;
 
   var size = (Math.sqrt(txtArea / charlen)); // * fudge
-  var sizeVH = size / window.innerHeight * 100;
+  var sizeVH = size / document.documentElement.clientHeight * 100;
   $txt.style.fontSize = sizeVH + 'vh';
 }
